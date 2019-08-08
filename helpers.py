362baa377fec1b3d00d56dbf88283e5fda8b27d0
walkenz1 <nile.walker@jhuapl.edu>
2019-08-08 19:58:26 -0400

    Beginnings of a Pooling Layer

--- helpers.py
+++ helpers.py
@@ -7,27 +7,49 @@ def sigmoidPrime(s):
     return s * (1 - s)
 activations = {"relu":ReLU,"sigmoid":sigmoid,"tanh":np.tanh,"threshold":0}
 activations_prime = {"sigmoid":sigmoidPrime}
-def MaxPooling(x,filter_shape=(3,3),padding = False):
-    # TODO: Import Code from my Mac
-    pass
+def MaxPooling(img,filter_shape=(2,2),stride=2, padding = False):
+    k = filter_shape
+    offset = int(k[0]/2)
+    x,y = offset,offset
+    stride = 1
+
+    x_ops = img.shape[0] - k[0] + k[0]%2 // stride 
+    y_ops = img.shape[1] - k[1] + k[1]%2 // stride
+    final = np.zeros((x_ops,y_ops))
+    for _y in range(y_ops):
+        for _x in range(x_ops):
+            final[_y][_x] = img[y-offset:y+offset+1][:,x-offset:x+offset+1].max()
+            x+=stride
+        x = offset
+        y+=stride
+    return final
 def Convolve(x,filters=[],padding=False):
     # TODO: Import Code from my Mac
     pass
+def scan():
+    k = (3,3)
+    outs = []
+    sample_img = np.zeros((20,20))
+    offset = int(k[0]/2)
+    x,y = offset,offset
+    stride = 1
+
+    x_ops = sample_img.shape[0] - k[0] + 1 // stride 
+    y_ops = sample_img.shape[1] - k[1] + 1 // stride
+    final = np.zeros((x_ops,y_ops))
+    for _y in range(y_ops):
+        for _x in range(x_ops):
+            outs.append(sample_img[y-offset:y+offset+1][:,x-offset:x+offset+1])
+            x+=stride 
+        x = offset
+        y+=stride
+    try:
+        assert(all([out.shape == k for out in outs]))
+        print(len(outs))
+    except AssertionError:
+        print(*[i.shape for i in outs], sep="\n")
+
 def flatten_img_list(x):
   return x.reshape((x.shape[0],x.shape[1] * x.shape[2]))
 if __name__ == "__main__":
-     def updateConfidence(self,pos, mod=.2, k_size=3):
-        if k_size%2 == 0:
-            print("Bad Kernel Size")
-            return None
-        x_lim = 8 #len(board[0])
-        y_lim = 8 #len(board)
-        offset = k_size//2
-        for x in range(pos[0] - offset, pos[0] + offset + 1):
-            for y in range(pos[1] - offset, pos[1] + offset + 1):
-                if (x < x_lim and y < y_lim):
-                    self.zone_confidence[y][x] = self.zone_confidence[y][x] + mod
-                    if self.zone_confidence[y][x] < 0: 
-                        self.zone_confidence[y][x] = 0 
-                    elif self.zone_confidence[y][x] > 1: 
-                        self.zone_confidence[y][x] = 1 
+    print(MaxPooling(np.zeros((16,16)),stride = 4,filter_shape=(4,4)))
--- layers.py
+++ layers.py
@@ -11,26 +11,58 @@ class Fully_Connected_Layer():
 
   def forward(self,X):
     if self.prev == None: 
-      self.activated_outputs = X
+      self.outputs = X
     else:
-      self.outputs = np.dot(X, self.weights) 
-      self.activated_outputs = self.activation(self.outputs)
-    return self.activated_outputs
+      self.unactivated_outputs = np.dot(X, self.weights) 
+      self.outputs = self.activation(self.unactivated_outputs)
+    return self.outputs
   def backward(self,y):
     if self.prev == None:
         return
     if self.next == None:
-        self.error = y - self.activated_outputs # error in output
+        self.error = y - self.outputs # error in output
     else: 
-        self.error = self.next.delta.dot(self.next.weights.T) 
-    self.delta = self.error*self.activation_prime(self.activated_outputs)
+        self.error = self.next.delta.dot(self.next.weights.T) # Represents the direction in which the weights of the current layer need to change in order to correct the error of the next forward layer 
+    self.delta = self.error*self.activation_prime(self.outputs)
     # TODO: Pass in learning rate from model  
-    self.weights += self.prev.activated_outputs.T.dot(self.delta) * .001  
+    self.weights += self.prev.outputs.T.dot(self.delta) * .001  
   def description(self): # Provide String representation to store the model 
     pass
+class PoolingLayer():
+    def __init__(self,filter_shape=(2,2), stride=2, padding = False):
+        self.next = None
+        self.prev = None
+        pass
+    def forward(self,X):
+        self.outputs = [] 
+        for img in X:
+            self.outputs.append(self.MaxPooling(img))
+        pass
+    def MaxPooling(self,img):
+        k = self.filter_shape
+        offset = int(k[0]/2)
+        x,y = offset,offset
+
+        x_ops = img.shape[0] - k[0] + k[0]%2 // self.stride 
+        y_ops = img.shape[1] - k[1] + k[1]%2 // self.stride
+        final = np.zeros((x_ops,y_ops))
+        for _y in range(y_ops):
+            for _x in range(x_ops):
+                final[_y][_x] = img[y-offset:y+offset+1][:,x-offset:x+offset+1].max()
+                x+=self.stride
+            x = offset
+            y+=self.stride
+        return final
+
+    def description(self): # Provide String representation to store the model 
+        pass 
 class ConvolutionalLayer():
     def __init__(self):
         self.__filters = []
+        pass
+    def forward(self):
+        pass
+    def backward(self):
         pass
     def description(self): # Provide String representation to store the model 
         pass
